74d4ad1267dabf1364d50f34e07daff3415f0588
Eugene Sokolov <eugine_s@ukr.net>
2015-01-26 17:22:40 +0200

    Added initial version of fixing Asus OPlay script.

---
+++ scripts/fix_asus.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/expect
+
+set timeout 20
+
+## connect
+spawn telnet 192.168.0.102
+
+## loging
+expect "AMP12 login:"
+end -- "root\r"
+expect "Enter 'help' for a list of built-in commands."
+
+## find lost disk
+send -- "rmmod ehci_hcd\r"
+sleep 120
+
+## check that the disk appeared
+send -- "mount\r"
+expect "/dev/scsi/host2/bus0/target0/lun0/part2 on /tmp/usbmounts/sda2 type ext3 (ro,recovery)" 
+
+## remount as RW disk
+send -- "umount /tmp/usbmounts/sda2\r"
+send -- "mount -t ext3 /dev/scsi/host2/bus0/target0/lun0/part2 /tmp/usbmounts/sda2\r"
+
+## verify mounting as RW
+send -- "mount\r"
+expect "/dev/scsi/host2/bus0/target0/lun0/part2 on /tmp/usbmounts/sda2 type ext3 (ro,recovery)"
+
+interact
